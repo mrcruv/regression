45faf2c46be6442d9574e54eaaf26f80b34a1072
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-04-23 15:47:36 +0200

    Multivariate linear regression
    sepal_length, sepal_width |-> 0, 1, 2

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import math
 
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn import datasets
+from sklearn.decomposition import PCA
 
 
 def linear_regression(dataset, targetset, weights, alpha):
@@ -11,7 +13,7 @@ def linear_regression(dataset, targetset, weights, alpha):
         for j in range(0, len(dataset)):
             sample = dataset[j]
             predicted = predict_lr(sample, weights)
-            expected = targetset[j][0]
+            expected = targetset[j]
             difference = expected - predicted
             if i == 0:
                 correction += difference
@@ -31,44 +33,51 @@ def predict_lr(sample, weights):
 def main():
     # import some data to play with
     iris = datasets.load_iris()
-    X = iris.data[:, 0:1]
-    y = iris.data[:, 1:2]
-    # y = iris.target
+    X = iris.data[:, :2]  # we only take the first two features.
+    y = iris.target
 
-    weights = [0, 0]
-    new_weights = linear_regression(X, y, weights, 0.1)
+    x1_min, x1_max = X[:, 0].min() - 0.5, X[:, 0].max() + 0.5
+    x2_min, x2_max = X[:, 1].min() - 0.5, X[:, 1].max() + 0.5
+    y_min, y_max = y.min() - 0.5, y.max() + 0.5
+
+    weights = [0, 0, 0]
     for i in range(0, 10000):
-        new_weights = linear_regression(X, y, new_weights, 0.0001)
-        print(new_weights)
+        weights = linear_regression(X, y, weights, 0.0001)
+        print(weights)
 
     n = len(X)
-    variance = 0
+    mse = 0
     for i in range(0, n):
         sample = X[i]
-        petal_length = sample[0]
-        petal_width = y[i][0]
-        predicted = predict_lr(sample, new_weights)
-        variance += math.fabs(petal_width - predicted)
-        print("petal lenght: "+str(petal_length)+" petal_width: "+str(petal_width)+" predicted: "+str(predicted))
-    variance /= n
-    print("y=("+str(round(new_weights[0], 2))+")+("+str(round(new_weights[1], 2))+")x "+"variance: "+str(variance))
+        expected = y[i]
+        predicted = round(predict_lr(sample, weights), 0)
+        mse += math.pow(expected - predicted, 2)
+        # print("expected: "+str(expected)+" predicted: "+str(predicted))
+    mse /= n
+    print("y=(" + str(round(weights[0], 2)) + ")+(" + str(round(weights[1], 2)) + ")x1+("+str(round(weights[2], 2))+")x2"+" mse: "+str(mse))
 
-    # x_min, x_max = X[:, 0].min() - 0.5, X[:, 0].max() + 0.5
-    # y_min, y_max = X[:, 1].min() - 0.5, X[:, 1].max() + 0.5
-    x_min, x_max = X.min() - 0.5, X.max() + 0.5
-    y_min, y_max = y.min() - 0.5, y.max() + 0.5
-    plt.figure(2, figsize=(8, 6))
     plt.clf()
-    # plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Set1, edgecolor="k")
-    plt.scatter(X[:, 0], y, c=y, cmap=plt.cm.Set1, edgecolor="k")
-    plt.xlabel("Sepal length")
-    plt.ylabel("Sepal width")
-    plt.xlim(x_min, x_max)
-    plt.ylim(y_min, y_max)
-    plt.xticks(())
-    plt.yticks(())
+    fig = plt.figure(1, figsize=(8, 6))
+    ax = Axes3D(fig, elev=-150, azim=110)
+    X_reduced = PCA(n_components=3).fit_transform(iris.data)
+    ax.scatter(
+        X_reduced[:, 0],
+        X_reduced[:, 1],
+        X_reduced[:, 2],
+        c=y,
+        cmap=plt.cm.Set1,
+        edgecolor="k",
+        s=40,
+    )
+    ax.set_title("First three PCA directions")
+    ax.set_xlabel("1st eigenvector")
+    ax.w_xaxis.set_ticklabels([])
+    ax.set_ylabel("2nd eigenvector")
+    ax.w_yaxis.set_ticklabels([])
+    ax.set_zlabel("3rd eigenvector")
+    ax.w_zaxis.set_ticklabels([])
 
-    plt.plot([x_min, x_max], [new_weights[0]+new_weights[1]*x_min, new_weights[0]+new_weights[1]*x_max], color="k")
+    plt.plot([x1_min, x1_max], [x2_min, x2_max], [weights[0]+weights[1]*x1_min+weights[2]*x2_min, (weights[0]+weights[1]*x1_max+weights[2]*x2_max)], color="k")
 
     plt.show()
 
